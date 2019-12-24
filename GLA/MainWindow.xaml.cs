21eeb87762d01054793315dd2463e8c4a58d3277
David <42221335+SmileyFtW@users.noreply.github.com>
2019-12-24 14:04:07 -0600

    Cleaned up file / changed to 8 players from 10
    Removed commented out code.
    Changed i loop to 8 players in anticipation of adding 8 to be able to score a match between 2 teams.

--- GLA/MainWindow.xaml.cs
+++ GLA/MainWindow.xaml.cs
@@ -30,41 +30,16 @@ namespace GLA
         private void btnLoadListWithNewPlayers_Click(object sender,RoutedEventArgs e)
         {
             MainUIHandler = new MainUIHandler();
-            for(int i=1;i<=10;i++)
+            for(int i=1;i<=8;i++)
             {
                 MainUIHandler.PlayerMgtHandler.AddNewPlayer("2020", "_", i, 3);
             }
-            /*
-            MainUIHandler.PlayerMgtHandler.AddNewPlayer("2020", "_", 1, 3);
-            MainUIHandler.PlayerMgtHandler.AddNewPlayer("2020", "_", 2, 3);
-            MainUIHandler.PlayerMgtHandler.AddNewPlayer("2020", "_", 3, 3);
-            */
+            
             foreach (IPlayer player in MainUIHandler.PlayerMgtHandler.NewPlayers)
             {
                 NewPlayers.Items.Add(player.PersonalInfo.NameOfPerson.FullName() + " - " + player.IDItem.ID);
             }
             
         }
-
-
     }
-    
-    /*
-    // For now going to use a console interface to get started
-    public class Program
-    {
-        public void Main()
-        {
-            IMainUIHandler UIHandlerMain = new MainUIHandler();
-            UIHandlerMain.PlayerMgtHandler.AddNewPlayer("2020", "_", 1, 3);
-            foreach(IPlayer player in UIHandlerMain.PlayerMgtHandler.NewPlayers)
-            {
-                Console.WriteLine(player.ID.ToString());
-                Console.WriteLine(player.PersonalInfo.NameOfPerson.FullName());
-                Console.ReadLine();
-            }
-                
-        }
-    }
-    */
 }
