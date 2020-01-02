86fae7c16c4ed5431dbe1eb403a9421a5d2889f2
SmileyFtW <dgmsmiles@gmail.com>
2020-01-02 15:23:12 -0600

    //Updated Testing for IDItem

--- GLA/Models/Shared/IDItem.cs
+++ GLA/Models/Shared/IDItem.cs
@@ -18,32 +18,87 @@ namespace GLA.Models
         {
             get
             {
-                if (HasID == true)
+                string result="";
+
+                if (InputIsEmpty(_prefix))
+                {
+                    result = "No Prefix";
+
+                    //return _prefix + _separator + _seqnumber.ToString().PadLeft(_seqnumberNumDigits, '0');
+                }
+                if(InputIsEmpty(_separator))
+                {
+                    if (result == "")
+                    {
+                        result = "No Separator";
+                    }
+                    else
+                    {
+                        result += " and No Separator";
+                    }
+                }
+
+                if (SeqNumberCompatibleWithNumberOfDigits(_seqnumber, _seqnumberNumDigits) == false)
+                {
+                    if (result != "")
+                    {
+                        result += " / ";
+                    }
+                    result += "Seq Number (" + _seqnumber + ") incompatible with Number of Digits (" + _seqnumberNumDigits + ")";
+                }
+
+                if (result == "")
                 {
                     return _prefix + _separator + _seqnumber.ToString().PadLeft(_seqnumberNumDigits, '0');
                 }
                 else
                 {
-                    return "";
+                    return result;
                 }
             }
         }
+        public bool InputIsEmpty(string input)
+        {
+            return (input == null || input=="");
+        }
+
+        public bool SeqNumberCompatibleWithNumberOfDigits(int SeqNumber, int NumberOfDigits)
+        {
+                if (NumberOfDigits > (SeqNumber.ToString().Length - 1))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+        } 
 
         public bool HasID { get; }
+        public bool IsValidID { get; }
 
         //Constructors
         public IDItem()
         {
             HasID = false;
+            IsValidID=false;
         }
         public IDItem (string Prefix, string Separator, int SeqNumber, int NumberOfDigits)
         {
-            _prefix = Prefix;
-            _separator = Separator;
-            _seqnumber = SeqNumber;
-            _seqnumberNumDigits = NumberOfDigits;
-            HasID = true;
+            if (SeqNumberCompatibleWithNumberOfDigits(SeqNumber, NumberOfDigits))
+            {
+                IsValidID = true;
+                HasID = true;
+                _prefix = Prefix;
+                _separator = Separator;
+                _seqnumber = SeqNumber;
+                _seqnumberNumDigits = NumberOfDigits;
+            }
+            else
+            {
+                IsValidID = false;
+                HasID = false;
+            }
         }
-        
     }
 }
