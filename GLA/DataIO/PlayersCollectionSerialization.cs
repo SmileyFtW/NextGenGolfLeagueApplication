84a18c768beee2aefbf731550090236267d8c743
SmileyFtW <42221335+SmileyFtW@users.noreply.github.com>
2020-01-09 16:28:42 -0600

    Update gitignore for Temp folder

--- GLA/DataIO/PlayersCollectionSerialization.cs
+++ GLA/DataIO/PlayersCollectionSerialization.cs
@@ -12,23 +12,29 @@ using System.Xml.Serialization;
 namespace GLA.DataIO
 {
     [XmlRoot("GolfLeaguePlayers")]
-    class PlayersCollection
+    class PlayersCollectionSerialization
     {
         [XmlArray("PlayerListing")]
         [XmlArrayItem("Player", typeof(Player))]
 
         public Hashtable Players { get; }
+        public string FilePath { get; private set; }
+        public string FileName { get; private set; }
 
         //Constructor
-        public PlayersCollection(Hashtable players)
+        public PlayersCollectionSerialization(Hashtable players)
         {
             Players = players;
+
         }
 
         public void SerializePlayersCollection(string outputFilePath, string outputFileName)
         {
             if (Players.Count > 0)
             {
+                FilePath = outputFilePath;
+                FileName = outputFileName;
+
                 XmlSerializer serializer = new XmlSerializer(Players.GetType());
                 TextWriter textwriter = new StreamWriter(outputFilePath + "/" + outputFileName + ".xml");
                 serializer.Serialize(textwriter, Players);
@@ -36,8 +42,10 @@ namespace GLA.DataIO
             }
         }
 
-        public Hashtable DeserializePlayersCollection(string filePath, string fileName)
+        public Hashtable DeserializePlayersCollection()
         {
+            
+            //TODO - write the code to get the file and turn it into a HaashTable
             Hashtable result=new Hashtable();
             return result;
         }
---
+++ GLA/Testing/Serialization/PlayerCollectionSerializationTests.cs
@@ -0,0 +1,50 @@
+﻿using GLA.DataIO;
+using GLA.Models;
+using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GLA.Testing.Serialization
+{
+    [TestFixture]
+    class PlayerCollectionSerializationTests
+    {
+        [Test]
+        public void PlayerCollection_SerializationOfOnePlayer_ReturnsValidOnePlayerXML()
+        {
+            //Arrange
+            //Player info
+            string playerFirstName = "David";
+            string playerLastName = "Miley";
+            string playerIDprefix = "Pre";
+            string playerIDsep = "Sep";
+            int playerIDseqNumber = 1;
+            int playerIDnumDigits = 3;
+
+            //Player object
+            Player player = new Player(playerIDprefix,playerIDsep,playerIDseqNumber,playerIDnumDigits);
+            player.PersonalInfo.NameOfPerson.First = playerFirstName;
+            player.PersonalInfo.NameOfPerson.Last = playerLastName;
+
+            //Players Collection
+            Hashtable playersCollection = new Hashtable();
+            playersCollection.Add(player.IDItem.ID.ToString(), player);
+
+            //Output path & file
+            string outputPath = @"C:\Users\dgmsm\Source\Repos\SmileyFtW\NextGenGolfLeagueApplication\GLA\DataIO\DataIOTemp\";
+            string outputFileName="TempPlayersXMLOutput";
+
+            //Serialization
+            PlayersCollectionSerialization playersCollectionSerialization = new PlayersCollectionSerialization(playersCollection);
+
+            //Act
+
+            //Assert
+        }
+
+    }
+}
